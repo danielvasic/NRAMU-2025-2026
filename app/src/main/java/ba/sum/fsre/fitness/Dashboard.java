7d388b4c209d5bb4df1a2af55445394bd23d8c4c
danielvasic <dvasic1@gmail.com>
2025-10-17 13:13:25 +0200

    Prve vježbe

---
+++ app/src/main/java/ba/sum/fsre/fitness/Dashboard.java
@@ -0,0 +1,14 @@
+package ba.sum.fsre.fitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+}
---
+++ app/src/main/java/ba/sum/fsre/fitness/LoginActivity.java
@@ -0,0 +1,26 @@
+package ba.sum.fsre.fitness;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button dashboardBtn = findViewById(R.id.dashboardBtn);
+        dashboardBtn.setOnClickListener(v -> {
+                startActivity(
+                        new Intent(LoginActivity.this, Dashboard.class)
+                );
+            }
+        );
+
+    }
+}
