3508e74525576a574a8dca12e498314d86f546a6
danielvasic <dvasic1@gmail.com>
2025-11-14 11:26:30 +0100

    Restrukturiran projekt - dodane vjezbe.

--- app/src/main/java/ba/sum/fsre/fitness/activities/DashboardActivity.java
+++ app/src/main/java/ba/sum/fsre/fitness/activities/DashboardActivity.java
@@ -2,15 +2,53 @@ package ba.sum.fsre.fitness.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import ba.sum.fsre.fitness.R;
+import ba.sum.fsre.fitness.utils.AuthManager;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    AuthManager authManager;
+    Button logoutBtn;
+    Button addExcerciseBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        authManager = new AuthManager(this);
+
+        initViews();
+        setupListeners();
+    }
+
+    private void initViews() {
+        logoutBtn = findViewById(R.id.logoutBtn);
+        addExcerciseBtn = findViewById(R.id.addExcerciseBtn);
+    }
+
+    private void setupListeners() {
+        logoutBtn.setOnClickListener(v -> {
+            authManager.logout();
+            startActivity(
+                    new Intent(
+                            this,
+                            LoginActivity.class
+                    )
+            );
+        });
+
+        addExcerciseBtn.setOnClickListener(v -> {
+            startActivity(
+                    new Intent(
+                            this,
+                            ExcerciseActivity.class
+                    )
+            );
+        });
     }
 }
---
+++ app/src/main/java/ba/sum/fsre/fitness/activities/ExcerciseActivity.java
@@ -0,0 +1,16 @@
+package ba.sum.fsre.fitness.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ba.sum.fsre.fitness.R;
+
+public class ExcerciseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_excercise);
+    }
+}
